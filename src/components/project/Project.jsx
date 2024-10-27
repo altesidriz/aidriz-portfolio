5ee94c7549dcc6d91b6bfafbf10484966517f452
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-10-27 15:37:04 +0200

    config

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,12 +4,21 @@ import styles from './project.module.css';
 const Project = ({item}) => {
   return (
     <div className={styles.single}>
+      {/* image */}
         <div className={styles.image}>
           <div className={styles.border}>
             <div className={styles.projectImg}>
               <img src={item.img} alt="" />
             </div>
             <img src="./monitor.png" alt="" />
+          </div>
+        </div>
+        {/* responsive image */}
+        <div className={styles.respImage}>
+          <div className={styles.respContent}>
+            <div className={styles.contentImg}>
+              <img src={item.img} alt="" />
+            </div>
           </div>
         </div>
         <div className={styles.description}>

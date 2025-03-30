18324e72ec982b38fc90864faedfcc3c3c73bada
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2025-03-30 11:51:40 +0300

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,11 +3,11 @@ import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  authDomain: "images-6e809.firebaseapp.com",
+  projectId: "images-6e809",
   storageBucket: "images-6e809.appspot.com",
-  messagingSenderId: import.meta.env.VITE_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  messagingSenderId: "383355911884",
+  appId: "1:383355911884:web:0449e55f0e25233f134c69"
 };
 
 // Initialize Firebase
